c8acef97b64935d7ea764b147b287dba14435ff2
iarla.crewe <iarla.crewe@gmail.com>
2023-08-02 21:10:14 +0100

    Add Initialize() and CloseAccount()

--- programs/attempt1/src/lib.rs
+++ programs/attempt1/src/lib.rs
@@ -6,10 +6,59 @@ declare_id!("4yhK57aLadt8wc9rmqF4QWDhVHQnkCMwXP8V6H9netGZ");
 pub mod attempt1 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(
+        ctx: Context<Initialize>
+    ) -> Result<()> {
+        let wallet = &mut ctx.accounts.wallet;
+        wallet.initializer = ctx.accounts.initializer.key();
+
+        Ok(())
+    }
+
+    pub fn close_account(
+        _ctx: Context<CloseAccount>
+    ) -> Result<()> {
+        msg!("Closed account");
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        seeds = [initializer.key().as_ref()],
+        bump,
+        payer = initializer,
+        space = Wallet::get_space()
+    )]
+    pub wallet: Account<'info, Wallet>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct CloseAccount<'info> {
+    #[account(
+        mut,
+        seeds = [initializer.key().as_ref()],
+        bump,
+        close = initializer
+    )]
+    pub wallet: Account<'info, Wallet>,
+    #[account(mut)]
+    pub initializer: Signer<'info>
+}
+
+#[account]
+pub struct Wallet {
+    pub initializer: Pubkey
+}
+
+impl Wallet {
+    pub fn get_space() -> usize {
+        8           // anchor discriminator
+        + 32        // initializer
+    }
+}
