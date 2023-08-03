1b675cd1522892ddf9e501db7209646900c4ed75
Nelo <nelo_66@protonmail.com>
2023-08-03 12:49:03 +0100

    Added the send_sol function

--- programs/attempt1/src/lib.rs
+++ programs/attempt1/src/lib.rs
@@ -1,9 +1,13 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::Transfer;
+//use solana_program::system_instruction::transfer;
 
 declare_id!("4yhK57aLadt8wc9rmqF4QWDhVHQnkCMwXP8V6H9netGZ");
 
 #[program]
 pub mod attempt1 {
+    use anchor_lang::solana_program::system_instruction;
+
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
@@ -16,6 +20,27 @@ pub mod attempt1 {
 
     pub fn close_account(_ctx: Context<CloseAccount>) -> Result<()> {
         msg!("Account closed");
+        Ok(())
+    }
+
+    pub fn send_sol(ctx: Context<SendLamports>, amount: u64) -> Result<()> {
+        msg!("Sending {} Lamports to {}", amount, ctx.accounts.receiver.key());
+
+        let from_account = &ctx.accounts.sender;
+        let to_account = &ctx.accounts.receiver;
+
+        let transfer_instruction = system_instruction::transfer(from_account.key, to_account.key, amount);
+
+        anchor_lang::solana_program::program::invoke_signed(
+            &transfer_instruction,
+            &[
+                from_account.to_account_info(),
+                to_account.clone(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            &[],
+        )?;
+
         Ok(())
     }
 }
@@ -46,6 +71,15 @@ pub struct CloseAccount<'info> {
     pub wallet: Account<'info, Wallet>,
     #[account(mut)]
     pub initializer: Signer<'info>
+}
+
+#[derive(Accounts)]
+pub struct SendLamports<'info> {
+    #[account(mut)]
+    pub sender: Signer<'info>,
+    #[account(mut)]
+    pub receiver: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
