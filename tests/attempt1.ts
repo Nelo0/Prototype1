5dba8789e7c6ec0e1eff32baaa14d5e4f98af9dc
iarla.crewe <iarla.crewe@gmail.com>
2023-08-03 12:47:42 +0100

    Add tests for initialize and close_account

--- tests/attempt1.ts
+++ tests/attempt1.ts
@@ -1,16 +1,28 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Attempt1 } from "../target/types/attempt1";
+import { expect } from "chai";
 
 describe("attempt1", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  let provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
 
-  const program = anchor.workspace.Attempt1 as Program<Attempt1>;
+  const program = anchor.workspace.Attempt1 as Program<Attempt1>
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const [walletPDA] = anchor.web3.PublicKey.findProgramAddressSync(
+    [provider.wallet.publicKey.toBuffer()],
+    program.programId
+  )
+
+  it("initialize", async () => {
+    const tx = await program.methods.initialize().rpc()
+
+    const account = await program.account.wallet.fetch(walletPDA)
+    expect(account.initializer === provider.wallet.publicKey)
   });
+
+  it("closeAccount", async () => {
+    const tx = await program.methods.closeAccount().rpc()
+  })
 });
